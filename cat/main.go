615579c011fbd9fcf1be015ae0f8404eb4ed6319
Jeremy Jackins <jeremyjackins@gmail.com>
2015-03-26 15:21:11 +0900

    cat: proper error reporting

--- cat/main.go
+++ cat/main.go
@@ -4,24 +4,32 @@ import (
 	"bufio"
 	"flag"
 	"io"
+	"log"
 	"os"
-
-	"sigint.ca/die"
 )
 
 func main() {
+	elog := log.New(os.Stderr, "cat: ", 0)
 	flag.Parse()
 	stdout := bufio.NewWriter(os.Stdout)
 	defer stdout.Flush()
 	if len(os.Args) == 1 {
-		io.Copy(stdout, bufio.NewReader(os.Stdin))
+		_, err := io.Copy(stdout, bufio.NewReader(os.Stdin))
+		if err != nil {
+			elog.Print(err)
+		}
 		return
 	}
 	for _, fname := range flag.Args() {
 		f, err := os.Open(fname)
-		die.On(err, "cat: error opening file")
+		if err != nil {
+			elog.Print(err)
+		}
 		fbuf := bufio.NewReader(f)
-		io.Copy(stdout, fbuf)
+		_, err = io.Copy(stdout, fbuf)
+		if err != nil {
+			elog.Print(err)
+		}
 		f.Close()
 	}
 }
