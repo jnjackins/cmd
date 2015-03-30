c19d764c2a19a01daf44c7a5484689a88db5064a
Jeremy Jackins <jeremyjackins@gmail.com>
2015-03-30 17:16:57 +0900

    sh: add l flag

--- sh/main.go
+++ sh/main.go
@@ -6,18 +6,34 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
+var lflag = flag.String("l", "", "read commands from `file` before reading normal input")
+
 func main() {
 	log.SetPrefix("sh: ")
 	log.SetFlags(0)
 	setupEnv()
-	in := bufio.NewReader(os.Stdin)
-	tty := isTTY(os.Stdin)
+	flag.Parse()
+	if *lflag != "" {
+		f, err := os.Open(*lflag)
+		if err != nil {
+			log.Print(err)
+		} else {
+			parse(f)
+		}
+	}
+	parse(os.Stdin)
+}
+
+func parse(f *os.File) {
+	in := bufio.NewReader(f)
+	tty := isTTY(f)
 	for {
 		if tty {
 			fmt.Print(os.Getenv("prompt"))
