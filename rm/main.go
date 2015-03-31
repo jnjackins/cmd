a8a4705c7125af5cdbba32f713f108f557c60522
Jeremy Jackins <jeremyjackins@gmail.com>
2015-03-31 10:24:58 +0900

    rm: f flag

--- rm/main.go
+++ rm/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 var rflag = flag.Bool("r", false, "recursive")
+var fflag = flag.Bool("f", false, "force")
 
 func main() {
 	elog := log.New(os.Stderr, "rm: ", 0)
@@ -20,17 +21,21 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
+	var status int
 	for _, path := range flag.Args() {
 		if *rflag {
 			err := os.RemoveAll(path)
-			if err != nil {
+			if err != nil && !*fflag {
 				elog.Print(err)
+				status++
 			}
 		} else {
 			err := os.Remove(path)
-			if err != nil {
+			if err != nil && !*fflag {
 				elog.Print(err)
+				status++
 			}
 		}
 	}
+	os.Exit(status)
 }
