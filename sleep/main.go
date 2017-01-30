69b0ee452abff22b6d941781e8398dcc62e50980
Jeremy Jackins <jeremy.jackins@meltwater.com>
2017-01-29 23:03:38 -0800

    sleep: parse durations
    Former-commit-id: c724bf80eea1145d7e72035e1c16ea84fca33afc

--- sleep/main.go
+++ sleep/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"log"
 	"os"
 	"strconv"
 	"time"
 )
 
 func main() {
-	if len(os.Args) >= 2 {
-		seconds, _ := strconv.Atoi(os.Args[1])
-		time.Sleep(time.Duration(seconds) * time.Second)
+	log.SetPrefix("sleep: ")
+	log.SetFlags(0)
+	if len(os.Args) > 1 {
+		var dur time.Duration
+		seconds, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			if dur, err = time.ParseDuration(os.Args[1]); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			dur = time.Duration(seconds) * time.Second
+		}
+		time.Sleep(dur)
 	}
 }
