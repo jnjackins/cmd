32bbf791be21dc4a99d693d230b1bb1cc104d58b
Jeremy Jackins <jeremy.jackins@meltwater.com>
2017-01-30 12:03:45 -0800

    rm: interactive mode
    Former-commit-id: 725dd1cf53b8433e52f8e7f290dfeabe65399b95

--- rm/main.go
+++ rm/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
-var rflag = flag.Bool("r", false, "recursive")
-var fflag = flag.Bool("f", false, "force")
-var rfflag = flag.Bool("rf", false, "recursive force")
+var (
+	rflag  = flag.Bool("r", false, "Recursive mode.")
+	fflag  = flag.Bool("f", false, "Force mode.")
+	rfflag = flag.Bool("rf", false, "-r and -f, for compatibility.")
+	iflag  = flag.Bool("i", false, "Interactive mode.")
+)
 
 func main() {
-	elog := log.New(os.Stderr, "rm: ", 0)
+	log.SetPrefix("rm: ")
+	log.SetFlags(0)
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, "Usage: rm [options] file ...")
 		flag.PrintDefaults()
@@ -28,16 +34,26 @@ func main() {
 	}
 	var status int
 	for _, path := range flag.Args() {
+		if *iflag {
+			fmt.Printf("remove %s? ", path)
+			s, err := bufio.NewReader(os.Stdin).ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			if !strings.HasPrefix(strings.ToLower(s), "y") {
+				continue
+			}
+		}
 		if *rflag {
 			err := os.RemoveAll(path)
 			if err != nil && !*fflag {
-				elog.Print(err)
+				log.Print(err)
 				status++
 			}
 		} else {
 			err := os.Remove(path)
 			if err != nil && !*fflag {
-				elog.Print(err)
+				log.Print(err)
 				status++
 			}
 		}
