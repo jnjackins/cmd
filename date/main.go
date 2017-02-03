a36ece0c5275e32495c5d36c1dceeba8dc447f72
Jeremy Jackins <jeremy.jackins@meltwater.com>
2017-02-02 19:46:32 -0800

    date: change formatting options

--- date/main.go
+++ date/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
-var nflag = flag.Bool("num", false, "Print the date and time in a numeric format")
-var fflag = flag.String("f", "", "Provide a custom Go time `format` (ref: Jan 2 3:04:05PM 2006 -0700)")
+var (
+	stamp  = flag.Bool("stamp", false, "Predefined format: "+time.Stamp)
+	rfc339 = flag.Bool("rfc3339", false, "Predefined format: "+time.RFC3339)
+	custom = flag.String("format", "", "Provide a custom Go time format (ref: Mon Jan 2 3:04:05PM 2006 -0700)")
+)
 
 func main() {
-	flag.Parse()
-	format := "Mon Jan 2 3:04:05 PM MST 2006"
-	if *nflag {
-		format = "2006-01-02 15:04:05 -07:00"
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [format option]\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "\nSee https://golang.org/pkg/time/#pkg-constants for formatting details.")
 	}
-	if *fflag != "" {
-		format = *fflag
+	flag.Parse()
+	format := time.UnixDate
+	if *stamp {
+		format = time.Stamp
+	}
+	if *rfc339 {
+		format = time.RFC3339
+	}
+	if *custom != "" {
+		format = *custom
 	}
 	fmt.Println(time.Now().Format(format))
 }
